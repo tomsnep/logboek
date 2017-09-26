64b06656ba61322116150cd93a7abfd8937bad0e
Tom Snepvangers <tom.snepvangers@tamtam.nl>
2017-09-26 17:41:56 +0200

    adding new content

--- source/javascript/src/modules/image-slider.js
+++ source/javascript/src/modules/image-slider.js
@@ -5,14 +5,20 @@ class ImageSlider {
 
 
 	constructor(element) {
-		this.$sliderEl = $( element );
+		this.$sliderEl 		= $( element );
+
+        this.$imageContainer = this.$sliderEl.find('.image-slider__slider-container');
+        this.$prevButton 	= this.$sliderEl.find('.image-slider__control--prev');
+        this.$nextButton 	= this.$sliderEl.find('.image-slider__control--next');
+
 		this.initSlider();
 	}
 
 	initSlider() {
-        this.$sliderEl.slick({
-
-		})
+        this.$imageContainer.slick({
+			prevArrow: this.$prevButton,
+            nextArrow: this.$nextButton
+        })
 	}
 
 
